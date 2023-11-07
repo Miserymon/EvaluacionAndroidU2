4e9375f0ad00cdc0212550b68f7cdc0bb26bb1d6
Miserymon <diegosalvo87@gmail.com>
2023-11-07 14:27:13 -0300

    Initial commit

---
+++ app/src/main/java/com/example/xansomniapruebita/ListaGuitars.java
@@ -0,0 +1,93 @@
+package com.example.xansomniapruebita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class ListaGuitars extends AppCompatActivity {
+    daoGuitar dao;
+    Adaptador adapter;
+    ArrayList<Guitar> lista;
+    Guitar guitar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_guitars);
+
+        dao = new daoGuitar(ListaGuitars.this);
+        lista = dao.verTodo();
+        adapter = new Adaptador(this, lista, dao);
+        ListView list = findViewById(R.id.list_guitar);
+        Button addGuitar = findViewById(R.id.btn_add_guitar);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            }
+        });
+
+        addGuitar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog = new Dialog(ListaGuitars.this);
+                dialog.setTitle("Nuevo Registro");
+                dialog.setCancelable(true);
+                dialog.setContentView(R.layout.dialog);
+                dialog.show();
+                final EditText guitarModel = dialog.findViewById(R.id.et_guitarmodel);
+                final EditText guitarBrand = dialog.findViewById(R.id.et_guitarbrand);
+                final EditText guitarType = dialog.findViewById(R.id.et_guitartype);
+                final EditText guitarPrice = dialog.findViewById(R.id.et_guitarprice);
+
+                final Spinner storeLocationSpinner = dialog.findViewById(R.id.sp_guitarstoreloca);
+                ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(
+                        ListaGuitars.this, R.array.ubicaciones, android.R.layout.simple_spinner_item);
+                spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                storeLocationSpinner.setAdapter(spinnerAdapter);
+
+                Button guardar = dialog.findViewById(R.id.btn_addtolist);
+                guardar.setText("Agregar Guitarra");
+                Button cancelar = dialog.findViewById(R.id.btn_cancelthisshit);
+                guardar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        try {
+                            String selectedLocation = storeLocationSpinner.getSelectedItem().toString();
+
+                            guitar = new Guitar(guitarModel.getText().toString(),
+                                    guitarBrand.getText().toString(),
+                                    guitarType.getText().toString(),
+                                    guitarPrice.getText().toString(),
+                                    selectedLocation);
+                            dao.insertar(guitar);
+                            lista = dao.verTodo();
+                            adapter.notifyDataSetChanged();
+                            dialog.dismiss();
+                        } catch (Exception e) {
+                            Toast.makeText(getApplication(), "ERROR", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+                cancelar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/xansomniapruebita/linksparavernana.java
@@ -0,0 +1,107 @@
+package com.example.xansomniapruebita;
+
+public class linksparavernana {
+    //IGNORAR ADSHJASDALKJ
+    //Aquí les paso los link de cada capítulo para poder ver los capítulos con la calidad buena que les decía ya que si es que llegan a borrarla aquí lo tienen asegurado, y de pasada poder verlo sin los anuncios molestos de la página Monoschinos! Disfrútenlo al máximo!!! *-*
+    //
+    //Capítulo 1: (https://ok.r
+    //u/video/1887541398186)
+    //Capítulo 2: (https://ok.r
+    //u/video/1887541856938)
+    //Capítulo 3: (https://ok.r
+    //u/video/1887544543914)
+    //Capítulo 4: (https://ok.r
+    //u/video/1887545199274)
+    //Capítulo 5: (https://ok.r
+    //u/video/1887551490730)
+    //Capítulo 6: (https://ok.r
+    //u/video/1887551949482)
+    //Capítulo 7: (https://ok.r
+    //u/video/1887553522346)
+    //Capítulo 8: (https://ok.r
+    //u/video/1887553850026)
+    //Capítulo 9: (https://ok.r
+    //u/video/1887554308778)
+    //Capítulo 10: (https://ok.r
+    //u/video/1887555357354)
+    //Capítulo 11: (https://ok.r
+    //u/video/1887557782186)
+    //Capítulo 11.5: (https://ok.r
+    //u/video/1887558240938) (Esta es una recopilación de los acontecimientos importantes desde el punto de vista del Pub de Junko, cómico por sus opiniones e interesante para volver a ver los momentos que a todos nos encantaron o a refrescar la memoria ^^)
+    //Capítulo 12: (https://ok.r
+    //u/video/1887559355050)
+    //Capítulo 13: (https://ok.r
+    //u/video/1887561058986)
+    //Capítulo 14: (https://ok.r
+    //u/video/1887562566314)
+    //Capítulo 15: (https://ok.r
+    //u/video/1887563090602)
+    //Capítulo 16: (https://ok.r
+    //u/video/1887564663466)
+    //Capítulo 17: (https://ok.r
+    //u/video/1887565253290)
+    //Capítulo 18: (https://ok.r
+    //u/video/1887566301866)
+    //Capítulo 19: (https://ok.r
+    //u/video/1887568005802)
+    //Capítulo 20: (https://ok.r
+    //u/video/1887574690474)
+    //Capítulo 21: (https://ok.r
+    //u/video/1887579277994)
+    //Capítulo 21.5: (https://ok.r
+    //u/video/1887594285738) (Segunda recopilación de los acontecimientos importantes desde el cap. 12 al 21 desde el punto de vista del Pub de Junko, cómico xd por sus opiniones e interesante para volver a ver los momentos que a todos nos encantaron-nos hicieron sufrir o a refrescar la memoria ^^)
+    //Capítulo 22: (https://ok.r
+    //u/video/1887596055210)
+    //Capítulo 23: (https://ok.r
+    //u/video/1887597431466)
+    //Capítulo 24: (https://ok.r
+    //u/video/1887611980458)
+    //Capítulo 25: (https://ok.r
+    //u/video/1887614601898)
+    //Capítulo 26: (https://ok.r
+    //u/video/1887615584938)
+    //Capítulo 27: (https://ok.r
+    //u/video/1887619648170)
+    //Capítulo 28: (https://ok.r
+    //u/video/1887626594986)
+    //Capítulo 29: (https://ok.r
+    //u/video/1887627512490)
+    //Capítulo 30: (https://ok.r
+    //u/video/1887629281962)
+    //Capítulo 31: (https://ok.r
+    //u/video/1887629806250)
+    //Capítulo 32: (https://ok.r
+    //u/video/1887631444650)
+    //Capítulo 33: (https://ok.r
+    //u/video/1887633017514)
+    //Capítulo 34: (https://ok.r
+    //u/video/1887636490922)
+    //Capítulo 35: (https://ok.r
+    //u/video/1887638457002)
+    //Capítulo 36: (https://ok.r
+    //u/video/1887639505578)
+    //Capítulo 36.5: (https://ok.r
+    //u/video/1887642061482) (Última recopilación de los acontecimientos desde cap. 22 al 36 desde el punto de vista del Pub de Junko, cómico por sus opiniones e interesante para volver a ver los momentos que a todos nos encantaron-nos hicieron sufrir o a refrescar la memoria ^^)
+    //Capítulo 37: (https://ok.r
+    //u/video/1887643372202)
+    //Capítulo 38: (https://ok.r
+    //u/video/1887645731498)
+    //Capítulo 39: (https://ok.r
+    //u/video/1887649860266)
+    //Capítulo 40: (https://ok.r
+    //u/video/1887651695274)
+    //Capítulo 41: (https://ok.r
+    //u/video/1887653202602)
+    //Capítulo 42: (https://ok.r
+    //u/video/1887655168682)
+    //Capítulo 43: (https://ok.r
+    //u/video/1887658052266)
+    //Capítulo 44: (https://ok.r
+    //u/video/1887659559594)
+    //Capítulo 45: (https://ok.r
+    //u/video/1887660346026)
+    //Capítulo 46: (https://ok.r
+    //u/video/1887662312106)
+    //Capítulo 47: (https://ok.r
+    //u/video/1887663688362)
+}
